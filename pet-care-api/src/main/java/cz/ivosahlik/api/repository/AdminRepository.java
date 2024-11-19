da0fd28aab7391b7a49ee350b4978c28bfb48c09
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-11-19 23:55:59 +0100

    refactoring

--- pet-care-api/src/main/java/cz/ivosahlik/api/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.ivosahlik.api.repository;
-
-import cz.ivosahlik.api.model.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
