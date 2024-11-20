e69a1acb0e1575220d94332954b8abb5ef1d0a99
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-11-20 17:23:18 +0100

    refactoring

--- pet-care-api/src/main/java/cz/ivosahlik/api/service/appointment/AppointmentService.java
+++ pet-care-api/src/main/java/cz/ivosahlik/api/service/appointment/AppointmentService.java
@@ -17,15 +17,17 @@ import cz.ivosahlik.api.service.pet.IPetService;
 import cz.ivosahlik.api.utils.FeedBackMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
