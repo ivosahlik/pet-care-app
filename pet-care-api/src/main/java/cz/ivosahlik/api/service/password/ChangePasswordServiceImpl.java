82dcd0c2da7750ceab1a4fb40b2fd0a419b6c935
Ivo Vošahlík, External <ivosahlik@seznam.cz>
2024-12-08 00:03:34 +0100

    refactoring, format

--- pet-care-api/src/main/java/cz/ivosahlik/api/service/password/ChangePasswordServiceImpl.java
+++ pet-care-api/src/main/java/cz/ivosahlik/api/service/password/ChangePasswordServiceImpl.java
@@ -18,19 +18,19 @@ public class ChangePasswordServiceImpl implements ChangePasswordService {
 
 
     @Override
-    public void changePassword(Long userId, ChangePasswordRequest request ) {
+    public void changePassword(Long userId, ChangePasswordRequest request) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
-         if(Objects.equals(request.getCurrentPassword(), "") || Objects.equals(request.getNewPassword(), "")) {
-             throw new IllegalArgumentException("All fields are required");
-         }
-         if(!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
-             throw new IllegalArgumentException("Current password does not match");
-         }
-         if(!request.getNewPassword().equals(request.getConfirmNewPassword())) {
-             throw new IllegalArgumentException("Password confirmation mis-match ");
-         }
-         user.setPassword(passwordEncoder.encode(request.getNewPassword()));
-         userRepository.save(user);
+        if (Objects.equals(request.getCurrentPassword(), "") || Objects.equals(request.getNewPassword(), "")) {
+            throw new IllegalArgumentException("All fields are required");
+        }
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("Current password does not match");
+        }
+        if (!request.getNewPassword().equals(request.getConfirmNewPassword())) {
+            throw new IllegalArgumentException("Password confirmation mis-match ");
+        }
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
 
     }
 }
